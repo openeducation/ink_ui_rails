5a8ea6eaf2af7d0c2582fe61713a2f57a4d6cf30
Matthew Burket <matthew@assignitapp.com>
2013-07-24 10:42:17 -0500

    Started Gem

---
+++ lib/ink_ui_rails.rb
@@ -0,0 +1,8 @@
+require "ink_ui_rails/version"
+
+module InkUiRails
+  module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+end
---
+++ lib/ink_ui_rails/version.rb
@@ -0,0 +1,3 @@
+module InkUiRails
+  VERSION = "2.1.1.0-pre0"
+end
